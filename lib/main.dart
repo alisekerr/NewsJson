f65d2729732a0b5907353bd822462ba660baf337
alisekerr <alisekerr2018@gmail.com>
2021-12-23 12:33:40 +0300

    23.12.2021

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(theme:newsThemeData ,debugShowCheckedModeBanner: false,home: HomeView());
